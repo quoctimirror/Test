340bbf9a332c05d23e29d58f4711e754c2038d84
quocti <ti.le@mirrorfuturediamond.com>
2025-07-09 17:37:09 +0700

    gud luck big boss

--- src/components/UniverseSection.jsx
+++ src/components/UniverseSection.jsx
@@ -3,104 +3,57 @@ import './universe_section.css';
 
 const UniverseSection = () => {
   return (
-    <div className="frame265-container">
-      <div className="frame265-frame265">
-        {/* Ảnh nền vẫn giữ ở ngoài */}
+    // Đã đổi tên class
+    <div className="universe-section-container">
+      {/* Đã đổi tên class */}
+      <div className="universe-section__viewport">
         <img
           src="/universe/rectangle229635-sx2i-2000w.png"
-          alt="Rectangle229635"
-          className="frame265-rectangle22"
+          alt="Starry background"
+          // Đã đổi tên class
+          className="universe-section__background-image"
         />
 
-        {/* BỌC TẤT CẢ CÁC YẾU TỐ ĐỒ HỌA */}
-        <div className="frame265-graphic-container">
-          <img
-            src="/universe/ellipse58644-i3f-200h.png"
-            alt="Ellipse58644"
-            className="frame265-ellipse5"
-          />
-          <img
-            src="/universe/ellipse88644-ai1s-300h.png"
-            alt="Ellipse88644"
-            className="frame265-ellipse8"
-          />
-          <img
-            src="/universe/ellipse98644-3eex-400h.png"
-            alt="Ellipse98644"
-            className="frame265-ellipse9"
-          />
-          <img
-            src="/universe/ellipse108644-assc-500h.png"
-            alt="Ellipse108644"
-            className="frame265-ellipse10"
-          />
-          <img
-            src="/universe/ellipse118644-eef-600h.png"
-            alt="Ellipse118644"
-            className="frame265-ellipse11"
-          />
-          <img
-            src="/universe/ellipse128644-asu-700h.png"
-            alt="Ellipse128644"
-            className="frame265-ellipse12"
-          />
-          <img
-            src="/universe/ellipse158645-2518r-200h.png"
-            alt="Ellipse158645"
-            className="frame265-ellipse15"
-          />
-          <img
-            src="/universe/ellipse178645-bp2c-200h.png"
-            alt="Ellipse178645"
-            className="frame265-ellipse17"
-          />
-          <img
-            src="/universe/ellipse168645-7rj-200h.png"
-            alt="Ellipse168645"
-            className="frame265-ellipse16"
-          />
-          <span className="frame265-text1 BTBeauSans_20pt_regular">
+        {/* Đã đổi tên class */}
+        <div className="universe-section__graphic-container">
+          <img src="/universe/ellipse58644-i3f-200h.png" alt="" className="universe-section__ellipse--center-1" />
+          <img src="/universe/ellipse88644-ai1s-300h.png" alt="" className="universe-section__ellipse--center-2" />
+          <img src="/universe/ellipse98644-3eex-400h.png" alt="" className="universe-section__ellipse--center-3" />
+          <img src="/universe/ellipse108644-assc-500h.png" alt="" className="universe-section__ellipse--center-4" />
+          <img src="/universe/ellipse118644-eef-600h.png" alt="" className="universe-section__ellipse--center-5" />
+          <img src="/universe/ellipse128644-asu-700h.png" alt="" className="universe-section__ellipse--center-6" />
+          
+          <img src="/universe/ellipse158645-2518r-200h.png" alt="" className="universe-section__dot-1" />
+          <img src="/universe/ellipse178645-bp2c-200h.png" alt="" className="universe-section__dot-2" />
+          <img src="/universe/ellipse168645-7rj-200h.png" alt="" className="universe-section__dot-3" />
+
+          {/* === ĐÃ ĐỔI TÊN CLASS VÀ ÁP DỤNG FONT MỚI === */}
+          <span className="universe-section__text">
             <span>MIRROR</span>
             <br />
             <span>EXPERIENCES</span>
           </span>
-          <div className="frame265-group107">
-            <img
-              src="/universe/ellipse13i864-m3pj-200h.png"
-              alt="Ellipse13I864"
-              className="frame265-ellipse13"
-            />
+          
+          <div className="universe-section__group-107">
+            <img src="/universe/ellipse13i864-m3pj-200h.png" alt="" className="universe-section__group-107-ellipse" />
           </div>
-          <div className="frame265-frame2661"></div>
-          <div className="frame265-frame2662">
-            <img
-              src="/universe/ellipse18i864-f59o-200h.png"
-              alt="Ellipse18I864"
-              className="frame265-ellipse18"
-            />
+          
+          <div className="universe-section__group-266-1"></div>
+          
+          <div className="universe-section__group-266-2">
+            <img src="/universe/ellipse18i864-f59o-200h.png" alt="" className="universe-section__group-266-2-ellipse" />
           </div>
-          <div className="frame265-frame268">
-            <img
-              src="/universe/ellipse7i864-vs3a-200h.png"
-              alt="Ellipse7I864"
-              className="frame265-ellipse7"
-            />
+          
+          <div className="universe-section__group-268">
+            <img src="/universe/ellipse7i864-vs3a-200h.png" alt="" className="universe-section__group-268-ellipse" />
           </div>
-          <div className="frame265-frame267">
-            <img
-              src="/universe/ellipse14i864-mguv-200h.png"
-              alt="Ellipse14I864"
-              className="frame265-ellipse14"
-            />
+          
+          <div className="universe-section__group-267">
+            <img src="/universe/ellipse14i864-mguv-200h.png" alt="" className="universe-section__group-267-ellipse" />
           </div>
-          <img
-            src="/universe/ellipse1699555-qgp-200h.png"
-            alt="Ellipse1699555"
-            className="frame265-ellipse169"
-          />
-        </div>
-        {/* KẾT THÚC VÙNG BỌC */}
 
+          <img src="/universe/ellipse1699555-qgp-200h.png" alt="" className="universe-section__ellipse--final" />
+        </div>
       </div>
     </div>
   );
