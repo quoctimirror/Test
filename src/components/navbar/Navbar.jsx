25f30e03370e0d5447a96f58351cd39b4caac9e6
dangnam1834 <nam.dang@mirrorfuturediamond.com>
2025-08-05 23:02:03 +0700

    commit for test button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,38 @@
 import "./Navbar.css";
 import MirrorLogo from "@assets/images/Mirror_Logo_new.svg";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className="navbar">
-      <div className="menu-wrapper">
-        <div className="menu-button">
+      <div className="menu-container">
+        <div 
+          className="menu-button"
+          onMouseEnter={() => setIsMenuOpen(true)}
+          onMouseLeave={() => setIsMenuOpen(false)}
+        >
           <span className="menu-text">Menu</span>
         </div>
-        <div className="menu-popup">
-          <ul className="menu-list">
-            <li>Products</li>
-            <li>Service & Support</li>
-            <li>About Mirror</li>
-            <li>News</li>
-            <li className="menu-divider"></li>
-            <li>Location</li>
-            <li>Contact us</li>
-            <li>Account</li>
-          </ul>
+        <div 
+          className={`menu-popup ${isMenuOpen ? 'active' : ''}`}
+          onMouseEnter={() => setIsMenuOpen(true)}
+          onMouseLeave={() => setIsMenuOpen(false)}
+        >
+          <div className="menu-groups">
+            <ul className="menu-list">
+              <li>Products</li>
+              <li>Service & Support</li>
+              <li>About Mirror</li>
+              <li>News</li>
+            </ul>
+            <ul className="menu-list">
+              <li>Location</li>
+              <li>Contact us</li>
+              <li>Account</li>
+            </ul>
+          </div>
         </div>
       </div>
       <img src={MirrorLogo} alt="Mirror Logo" className="navbar-logo" />
@@ -26,9 +40,7 @@ export default function Navbar() {
         <a href="#account" className="account-link">
           Account
         </a>
-        <button className="immersive-button">
-          Immersive Showroom
-        </button>
+        <button className="immersive-button">Immersive Showroom</button>
       </div>
     </div>
   );
