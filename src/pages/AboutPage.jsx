cb92c605358316d6b2cbe2ad37ca6bc51d97d255
dangnam1834 <nam.dang@mirrorfuturediamond.com>
2025-08-19 10:24:45 +0700

    commit for test section overlap effect on scroll

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import SloganSection from "../components/about/sloganSection/SloganSection";
 import StartingPlaceSection from "../components/about/startingPlaceSection/StartingPlaceSection";
 import IntroBOD from "../components/about/introBOD/IntroBOD";
@@ -10,18 +12,63 @@ import SharedSection from "../components/about/sharedSection/SharedSection";
 import DiscoverSection from "../components/about/discoverSection/DiscoverSection";
 import "./AboutPage.css";
 
+gsap.registerPlugin(ScrollTrigger);
+
 const AboutPage = () => {
+  useEffect(() => {
+    let panels = gsap.utils.toArray(".panel");
+
+    panels.forEach((panel) => {
+      ScrollTrigger.create({
+        trigger: panel,
+        start: () => panel.offsetHeight < window.innerHeight ? "top top" : "bottom bottom",
+        pin: true, 
+        pinSpacing: false 
+      });
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+  }, []);
+
   return (
     <div className="about-page">
-      <SloganSection />
-      <StartingPlaceSection />
-      <IntroBOD />
-      <BODMember />
-      <AtMirror />
-      <MirrorverseSection />
-      <MirrorNetworkSection />
-      <DiscoverSection />
-      <SharedSection />
+      <section className="panel">
+        <SloganSection />
+      </section>
+      
+      <section className="panel">
+        <StartingPlaceSection />
+      </section>
+      
+      <section className="panel">
+        <IntroBOD />
+      </section>
+      
+      <section className="panel">
+        <BODMember />
+      </section>
+      
+      <section className="panel">
+        <AtMirror />
+      </section>
+      
+      <section className="panel">
+        <MirrorverseSection />
+      </section>
+      
+      <section className="panel">
+        <MirrorNetworkSection />
+      </section>
+      
+      <section className="panel">
+        <DiscoverSection />
+      </section>
+      
+      <section className="panel">
+        <SharedSection />
+      </section>
     </div>
   );
 };
