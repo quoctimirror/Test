db58cc6f75320e0b17075645e4dbf6d2b371206c
dangnam1834 <nam.dang@mirrorfuturediamond.com>
2025-08-20 10:28:10 +0700

    commit for test animation in future diamond

--- src/components/home-page/futureDiamond/FutureDiamond.jsx
+++ src/components/home-page/futureDiamond/FutureDiamond.jsx
@@ -12,14 +12,14 @@ const FutureDiamond = () => {
         <h1 className="future-diamond-title">Future Diamond</h1>
         
         <div className="future-diamond-description">
-          <p className="description-text">
+          <h1 className="heading-1--no-margin">
             The world's newest diamond cut, a<br />
             new star is born. Its 91 facets sparkle<br />
             the brightest, emitting a fire like no<br />
             other in a celebration of the<br />
             constellations and the extraordinary<br />
             potential of mankind.
-          </p>
+          </h1>
         </div>
       </div>
     </section>
