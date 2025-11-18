6f5503bbfaccfe5ef431484cca155553789d9a46
quoctimirror <ti.le@mirrorfuturediamond.com>
2025-11-18 11:48:50 +0700

    passby thumb finger

--- src/components/ijewelTryOn/ijewel_useARTryOn.js
+++ src/components/ijewelTryOn/ijewel_useARTryOn.js
@@ -393,7 +393,14 @@ export const useIJewelARTryOn = ({ canvasRef, modelName, onError, onModelLoad })
     flipCamera: () => tryonRef.current?.flipCamera(),
     switchFinger: () => {
       const tryon = tryonRef.current;
-      if (tryon) tryon.finger = (tryon.finger + 1) % 5;
+      if (tryon) {
+        let newFinger = (tryon.finger + 1) % 5;
+        // Skip ngón cái (index 2)
+        if (newFinger === 2) {
+          newFinger = 3;
+        }
+        tryon.finger = newFinger;
+      }
     },
     saveImage: () => tryonRef.current?.saveImage(),
     viewer: viewerRef.current,
