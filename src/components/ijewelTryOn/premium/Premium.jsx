62ba1261bcf8e6eccbaa39601ce93486c6020bcb
mirror_quocti <ti.le@mirrorfuturediamond.com>
2025-12-24 04:43:11 +0000

    fix: change camera icon button

--- src/components/ijewelTryOn/premium/Premium.jsx
+++ src/components/ijewelTryOn/premium/Premium.jsx
@@ -458,8 +458,8 @@ const Premium = () => {
             {/* Capture button */}
             <button className={styles.circleButton} onClick={handleCapture} aria-label="Capture Photo">
               <svg className={styles.buttonIcon} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <circle cx="12" cy="12" r="10" />
-                <circle cx="12" cy="12" r="6" fill="currentColor" />
+                <path d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z" />
+                <circle cx="12" cy="13" r="3" />
               </svg>
             </button>
           </div>
