40e6194703b426fa0ef573ec1950243e42966150
quoctimirror <ti.le@mirrorfuturediamond.com>
2026-01-28 10:43:51 +0700

    fixed bug solock for different display scale (#181)
    * fixed bug solock for different display scale
    
    * checkpoint 28012026 1040

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,9 +1,6 @@
 import { useEffect, useState } from "react";
 import { getImageUrl } from "@utils/cloudflareMediaUtil";
-import ScrollDownArrow from "@components/common/button/ScrollDownArrow";
 import ScrollToTopArrow from "@components/common/button/ScrollToTopArrow";
-import ImmersiveButton from "@components/common/button/ImmersiveButton";
-import { useScrollToNextSection } from "@/hooks/useScrollToNextSection";
 import { useBottomTheme } from "@/hooks/useBottomTheme";
 import "@components/home-page/scrollEffect/ScrollEffect.css";
 import "./WelcomePage.css";
@@ -11,21 +8,12 @@ import "./WelcomePage.css";
 const WelcomePage = () => {
   const [isLoaded, setIsLoaded] = useState(false);
 
-  const { isArrowVisible, handleArrowClick } = useScrollToNextSection({
-    footerSelector: '.footer',
-  });
   const { theme: arrowTheme } = useBottomTheme();
-  const [isImmersiveCollapsed, setIsImmersiveCollapsed] = useState(false);
   const [showScrollTop, setShowScrollTop] = useState(false);
-
-  const handleImmersiveClick = () => {
-    console.log("Immersive button clicked");
-  };
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollY = window.scrollY;
-      setIsImmersiveCollapsed(scrollY > 100);
       setShowScrollTop(scrollY > 500);
     };
     window.addEventListener("scroll", handleScroll, { passive: true });
@@ -61,22 +49,6 @@ const WelcomePage = () => {
           Presence.
         </h2>
       </div>
-
-      {/* Fixed Immersive Button */}
-      <div className="fixed-immersive-container">
-        <ImmersiveButton
-          theme={arrowTheme}
-          isCollapsed={isImmersiveCollapsed}
-          onClick={handleImmersiveClick}
-        />
-      </div>
-
-      {/* Fixed Arrow Button */}
-      {isArrowVisible && (
-        <div className="fixed-arrow-container">
-          <ScrollDownArrow theme={arrowTheme} onClick={handleArrowClick} />
-        </div>
-      )}
 
       {/* Fixed Scroll to Top Button - only show when scroll-down arrow is hidden */}
       <div className={`fixed-scroll-top-container ${showScrollTop ? 'visible' : ''}`}>
